fbb8b9150f5eff7701ea1b7e81f047bc6ce74845
unknown <felix.gagne.cliche@gmail.com>
2024-09-08 11:13:51 -0400

    test action

--- .github/actions/setup-godot/entrypoint.sh
+++ .github/actions/setup-godot/entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/sh -l
-# echo "Godot Version: $1.$2"
-# curl -L https://github.com/godotengine/godot/releases/download/$1-$2/Godot_v$1-$2_linux.x86_64.zip > godot.zip
-# curl -L https://github.com/godotengine/godot/releases/download/$1-$2/Godot_v$1-$2_export_templates.tpz > templates.tpz
+echo "Godot Version: $1.$2"
+curl -L https://github.com/godotengine/godot/releases/download/$1-$2/Godot_v$1-$2_linux.x86_64.zip > godot.zip
+curl -L https://github.com/godotengine/godot/releases/download/$1-$2/Godot_v$1-$2_export_templates.tpz > templates.tpz
 
-# unzip godot.zip
-# unzip templates.tpz
+unzip godot.zip
+unzip templates.tpz
 
-# mkdir -vp ~/.local/share/godot/export_templates/${GODOT_VERSION}.${GODOT_PATCH_VERSION}
-# mv templates/* ~/.local/share/godot/export_templates/${GODOT_VERSION}.${GODOT_PATCH_VERSION}
+mkdir -vp ~/.local/share/godot/export_templates/${GODOT_VERSION}.${GODOT_PATCH_VERSION}
+mv templates/* ~/.local/share/godot/export_templates/${GODOT_VERSION}.${GODOT_PATCH_VERSION}
 
-# mv Godot_v${GODOT_VERSION}-${GODOT_PATCH_VERSION}_linux.x86_64 /usr/local/bin/godot
+mv Godot_v${GODOT_VERSION}-${GODOT_PATCH_VERSION}_linux.x86_64 /usr/local/bin/godot
 
-# rm -R templates/
-# rm templates.tpz godot.zip
-# ls -la /usr/local/bin
-
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+rm -R templates/
+rm templates.tpz godot.zip
+ls -la /usr/local/bin
